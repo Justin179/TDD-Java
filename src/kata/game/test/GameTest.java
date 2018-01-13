610e9f61f85d3f0edd9fc3154c60d8df8fbd38d1
Justin Chen <justin.uob@gmail.com>
2018-01-13 16:10:30 +0800

    1-3 ok

--- src/kata/game/test/GameTest.java
+++ src/kata/game/test/GameTest.java
@@ -1,24 +1,23 @@
 package kata.game.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.*;
-import org.junit.*;
-import kata.game.*;
+import org.junit.Test;
 
 import kata.game.Game;
 
-class GameTest {
+import static org.junit.Assert.*;
+import org.junit.*;
 
-	private Game g = new Game();		// 宣告參考變數，指向 Game instance
+public class GameTest {
+
+	private Game g;		// 宣告參考變數，指向 Game instance
 	
-//	@Before
-//	public void setUp() throws Exception {
-//		g = new Game();
-//	}
+	@Before
+	public void setUp() throws Exception {
+		g = new Game();
+	}
 
 	@Test
 	// 測試玩家投球共20次全洗溝 (gutter)時的總得分數
@@ -27,8 +26,21 @@ class GameTest {
 		int expected = 0;	// 期望結果值
 		int actual;			// 實際結果值
 		
-		// 投20次，每次倒0瓶
 		this.rollMany(20, 0);
+		actual = g.計分();
+		
+		// 斷言比對
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	// 測試玩家投球共20次，每次都只得一分時的總得分數
+	// 期望結果值：20
+	public void testAllOnes() throws Exception {
+		int expected = 20;
+		int actual;	
+		
+		this.rollMany(20, 1);
 		actual = g.計分();
 		
 		// 斷言比對
@@ -38,21 +50,26 @@ class GameTest {
 	private void rollMany(int n, int pins) {
 		for (int i = 0; i < n; i++)
 			g.投球(pins);
-	}	
-	
-	@Test
-	// 測試玩家投球共20次，每次都只得一分時的總得分數
-	// 期望結果值：20	
-	public void testAllOnes() throws Exception {
-		int expected = 20;
-		int actual;	
+	}
 		
-		// 投20次，每次倒1瓶
-		this.rollMany(20, 1);
+	@Test
+	// 測試玩家整局只有一次補中+次一計分格投球的分數
+	// 共 20 次投球機會，其餘 17 次皆0分	
+	public void testOneSpare() throws Exception {
+		int expected = 16;
+		int actual;
+		
+		this.rollSPare();	 //補中
+		g.投球(3);
+		this.rollMany(17,0); //其餘17次投球皆0分
 		actual = g.計分();
 		
-		// 斷言比對
 		assertEquals(expected, actual);
 	}
+	
+	private void rollSPare() {
+		g.投球(5);
+		g.投球(5);
+	}	
 
 }

a52fdec59a146b65a94e06b31715b1714a88da59
Justin Chen <justin.uob@gmail.com>
2018-01-13 16:05:55 +0800

    1_2

--- src/kata/game/Game.java
+++ src/kata/game/Game.java
@@ -1,10 +1,13 @@
 package kata.game;
 
 public class Game {
-	public void 投球(int 瓶數){		
+	private int score = 0;
+	
+	public void 投球(int 瓶數){
+		score += 瓶數;
 	}
 	
 	public int 計分(){
-		return -1;
+		return score;
 	}
 }
--- src/kata/game/test/GameTest.java
+++ src/kata/game/test/GameTest.java
@@ -2,23 +2,57 @@ package kata.game.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import kata.game.*;
+
+import kata.game.Game;
 
 class GameTest {
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+	private Game g = new Game();		// 宣告參考變數，指向 Game instance
+	
+//	@Before
+//	public void setUp() throws Exception {
+//		g = new Game();
+//	}
 
 	@Test
-	void test投球() {
-//		fail("Not yet implemented");
+	// 測試玩家投球共20次全洗溝 (gutter)時的總得分數
+	// 期望結果值：0
+	public void testGutterGame() throws Exception {
+		int expected = 0;	// 期望結果值
+		int actual;			// 實際結果值
+		
+		// 投20次，每次倒0瓶
+		this.rollMany(20, 0);
+		actual = g.計分();
+		
+		// 斷言比對
+		assertEquals(expected, actual);
 	}
-
+	
+	private void rollMany(int n, int pins) {
+		for (int i = 0; i < n; i++)
+			g.投球(pins);
+	}	
+	
 	@Test
-	void test計分() {
-//		fail("Not yet implemented");
+	// 測試玩家投球共20次，每次都只得一分時的總得分數
+	// 期望結果值：20	
+	public void testAllOnes() throws Exception {
+		int expected = 20;
+		int actual;	
+		
+		// 投20次，每次倒1瓶
+		this.rollMany(20, 1);
+		actual = g.計分();
+		
+		// 斷言比對
+		assertEquals(expected, actual);
 	}
 
 }

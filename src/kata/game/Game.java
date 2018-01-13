77c07c928334f4debbf2d894599463dd1ba3117a
Justin Chen <justin.uob@gmail.com>
2018-01-13 14:54:41 +0800

    BowlingGame 骨架

---
+++ src/kata/game/Game.java
@@ -0,0 +1,10 @@
+package kata.game;
+
+public class Game {
+	public void 投球(int 瓶數){		
+	}
+	
+	public int 計分(){
+		return -1;
+	}
+}
---
+++ src/kata/game/test/GameTest.java
@@ -0,0 +1,24 @@
+package kata.game.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test投球() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	void test計分() {
+//		fail("Not yet implemented");
+	}
+
+}
